6372f970ffc7875bd1cec1bd56eab544aea43b7c
tgummerer <t.gummerer@gmail.com>
2011-10-10 11:24:30 -0400

    Added Monitor apps button

--- gen/com/tgummerer/R.java
+++ gen/com/tgummerer/R.java
@@ -23,6 +23,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int main_monitor=0x7f040004;
         public static final int main_showDiagram=0x7f040003;
         public static final int main_startTests=0x7f040002;
     }
