692e226dac81bbbe649104fe350adff038180aea
tgummerer <t.gummerer@gmail.com>
2011-10-11 10:13:19 -0400

    Give credit to androidicons.com

--- gen/com/tgummerer/R.java
+++ gen/com/tgummerer/R.java
@@ -29,12 +29,14 @@ public final class R {
         public static final int tableRow1=0x7f060001;
         public static final int tableRow2=0x7f060004;
         public static final int textView1=0x7f060000;
+        public static final int textView2=0x7f060007;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int home_credit=0x7f040005;
         public static final int home_monitor_applications=0x7f040003;
         public static final int home_show_diagram=0x7f040002;
         public static final int home_show_monitor_diagram=0x7f040004;
