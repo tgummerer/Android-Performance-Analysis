c031cfda747c6bf6939bf44e55af1064534ab779
tgummerer <t.gummerer@gmail.com>
2011-11-07 17:48:06 +0100

    Added ant file, for console compiling and installing

---
+++ gen/R.java.d
@@ -0,0 +1,15 @@
+/Users/tommy/Documents/workspace/PerformanceAnalysis/gen/com/tgummerer/R.java \
+ : /Users/tommy/Documents/workspace/PerformanceAnalysis/res/drawable-hdpi/creep002.png \
+/Users/tommy/Documents/workspace/PerformanceAnalysis/res/drawable-hdpi/creep003.png \
+/Users/tommy/Documents/workspace/PerformanceAnalysis/res/drawable-hdpi/creep006.png \
+/Users/tommy/Documents/workspace/PerformanceAnalysis/res/drawable-hdpi/creep007.png \
+/Users/tommy/Documents/workspace/PerformanceAnalysis/res/drawable-hdpi/creep008.png \
+/Users/tommy/Documents/workspace/PerformanceAnalysis/res/drawable-hdpi/diagram.png \
+/Users/tommy/Documents/workspace/PerformanceAnalysis/res/drawable-hdpi/monitor.png \
+/Users/tommy/Documents/workspace/PerformanceAnalysis/res/drawable-hdpi/starttests.png \
+/Users/tommy/Documents/workspace/PerformanceAnalysis/res/drawable-mdpi/icon.png \
+/Users/tommy/Documents/workspace/PerformanceAnalysis/res/layout/main.xml \
+/Users/tommy/Documents/workspace/PerformanceAnalysis/res/layout/progress.xml \
+/Users/tommy/Documents/workspace/PerformanceAnalysis/res/values/strings.xml \
+/Users/tommy/Documents/workspace/PerformanceAnalysis/res/values/styles.xml \
+/Users/tommy/Documents/workspace/PerformanceAnalysis/AndroidManifest.xml \
