00ceae11d323d05b57cd1f5fe1000010c594257d
tgummerer <t.gummerer@gmail.com>
2011-11-28 16:03:09 +0100

    Drawing the time diagram

--- jni/diagram.cpp
+++ jni/diagram.cpp
@@ -7,7 +7,9 @@
  */
 
 #include "diagram.h"
-#include "connection.h"
+#include "sqlite3.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 Diagram::Diagram()
 {
@@ -42,9 +44,42 @@ void Diagram::drawAxis()
 
 void Diagram::drawBars()
 {
-    o->setRGBColor(120, 120, 255, 0);
-    o->drawRectangle(0.85, -0.83, -0.6, 0.2);
-    o->setRGBColor(255, 120, 120, 1);
-    o->drawRectangle(0.85, -0.81, -0.4, 0.2);
+    const char countquery[35] = "select count(*) from measurements";
+    sqlite3_stmt * pstmt = c->prepare(countquery);
+    int nrtests = 0;
+    if (sqlite3_step(pstmt) == SQLITE_ROW)
+        nrtests = atoi((char *)sqlite3_column_text(pstmt, 0));
 
+    const char maxquery[60] = "select max(time) from measurements";
+    pstmt = c->prepare(maxquery);
+    int maxtime = 0;
+    if (sqlite3_step(pstmt) == SQLITE_ROW)
+        maxtime = atoi((char *)sqlite3_column_text(pstmt, 0));
+
+    const char selectquery[200] = "select langid, algorithmid, time from measurements order by algorithmid desc, langid asc";
+    pstmt = c->prepare(selectquery);
+
+    float barwidth = ((this->val(LEFT) + this->val(RIGHT)) - (nrtests + nrtests / 2) * DISTANCE) / (nrtests / 2);
+    
+    int i = 0;
+    for (; i < nrtests; ++i)
+    {
+        if(!sqlite3_step(pstmt) == SQLITE_ROW)
+            break; // Should never happen, just for safety
+        int langid = atoi((char *)sqlite3_column_text(pstmt, 0));
+        if (langid == 1)
+            o->setRGBColor(120, 120, 255, 0);
+        else
+            o->setRGBColor(255, 120, 120, 1);
+
+        o->drawRectangle(BOTTOM, LEFT + (float)(i + 1) * DISTANCE + (i / 2) * barwidth, MAX_HEIGHT / maxtime * atoi((char *)sqlite3_column_text(pstmt, 2)), barwidth);
+
+    }
+}
+
+float Diagram::val(float f)
+{
+    if (f < 0)
+        return -f;
+    return f;
 }
--- jni/diagram.h
+++ jni/diagram.h
@@ -12,12 +12,21 @@
 #include "gllib.h"
 #include "connection.h"
 
+#define LEFT       -0.85
+#define RIGHT       0.85
+
+#define BOTTOM      0.85
+#define MAX_HEIGHT -1.7
+
+#define DISTANCE    0.02
+
 class Diagram {
     private:
         OpenGL * o;
         Connection * c;
         void drawAxis();
         void drawBars();
+        float val(float);
     public:
         Diagram();
         ~Diagram();
